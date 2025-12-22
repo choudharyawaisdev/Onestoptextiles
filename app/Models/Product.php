2bc42f5f1769056d0b24f27a01cd4eeb59168e0d
Choudhary Awais <153897148+choudharyawaisdev@users.noreply.github.com>
2025-12-22 12:54:14 +0500

    changes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+   protected $fillable = [
+    'name',
+    'slug',
+    'description',
+    'category',
+    'material',
+    'is_active'
+];
+
 }
--- app/Models/ProductDetail.php
+++ app/Models/ProductDetail.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductDetail extends Model
 {
-    //
+   protected $fillable = [
+    'product_id',
+    'size',
+    'width',
+    'length',
+    'unit',
+    'color',
+    'price',
+    'currency',
+    'images',
+    'stock',
+    'weight',
+    'is_custom_size',
+    'notes'
+];
+    protected $casts = [
+        'images' => 'array',
+        'is_custom_size' => 'boolean'
+    ];
+
 }
